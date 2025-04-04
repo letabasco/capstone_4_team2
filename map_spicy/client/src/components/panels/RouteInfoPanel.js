65978c233499b8aba791f3db3295f9d3495aab3a
letabasco <brax2408@gmail.com>
2025-04-04 14:26:27 +0900

    위젯버튼 비활성화 클래스

--- map_spicy/client/src/components/panels/RouteInfoPanel.js
+++ map_spicy/client/src/components/panels/RouteInfoPanel.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:727db2c9d79745fce6b80d23d4531c2b8f616363de32cdfb77ab7d38c7083d09
-size 3726
+oid sha256:4173d80549e254112761b93ae5bbf058b4098335eb7bc3073791bbef9cb52ead
+size 4039
