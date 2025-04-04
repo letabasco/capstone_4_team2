e78a861428f00364992e97bc560a46da57514f7a
letabasco <brax2408@gmail.com>
2025-04-04 20:23:03 +0900

    지도에서 마커 flase로 초기 설정

--- map_spicy/client/src/components/search/RouteSelectionScreen.js
+++ map_spicy/client/src/components/search/RouteSelectionScreen.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c7c01dcb8be158d77228581bcec8ded401feb2d05e40637d1c0fe988a50cf06
-size 5092
+oid sha256:c331fe6aeea99ed03ed51040d2ed5289eb3d4bff57065a1476a639cb7913e211
+size 5242
--- map_spicy/client/src/services/RouteService.js
+++ map_spicy/client/src/services/RouteService.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bee07cace98993fe003347578825eeadb81aa24e41240cdb705729f9a6b4362
-size 8883
+oid sha256:63f6de72a84228268b7067b2f34df2c69200ef384cc671886b5ee438739ac2d9
+size 9131
